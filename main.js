bdb527aefe642d0690249b9171d26ec4e8da9f99
Ian Halcomb <halcombian@gmail.com>
2022-08-18 01:59:44 -0400

    Give add task btn functionality to create task cards

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-const startBtn = document.getElementById("start-project");
-startBtn.addEventListener("click", () => {
-	startBtn.style.visibility = "hidden";
+//Gets projectCon to be used in createProjectWindow() and createProjectCards()
+const projectCon = document.getElementById("project-con");
+const startProjectBtn = document.getElementById("start-project");
+startProjectBtn.addEventListener("click", () => {
+	startProjectBtn.style.visibility = "hidden";
 	createProjectWindow();
 	addBtnListener();
 });
 
-//Gets project con to be used in createProjectWindow() and createProjectCards()
-const projectCon = document.getElementById("project-con");
+const taskCon = document.getElementById("task-con");
+const startTaskBtn = document.getElementById("start-task");
+startTaskBtn.addEventListener("click", () => {
+	startTaskBtn.style.visibility = "hidden";
+	createTaskWindow();
+	addTaskBtnListener();
+});
 
 function createProjectWindow() {
 	const createWindow = document.createElement("div");
 	createWindow.id = "create-window";
-	projectCon.appendChild(createWindow);
+	projectCon.insertBefore(createWindow, startProjectBtn);
 
 	const createForm = document.createElement("form");
 	createWindow.appendChild(createForm);
@@ -36,6 +43,34 @@ function createProjectWindow() {
 	createForm.appendChild(addBtn);
 }
 
+function createTaskWindow() {
+	const createWindow = document.createElement("div");
+	createWindow.id = "create-window";
+	taskCon.insertBefore(createWindow, startTaskBtn);
+
+	const createForm = document.createElement("form");
+	createWindow.appendChild(createForm);
+
+	const descriptionInput = document.createElement("input");
+	descriptionInput.type = "text";
+	descriptionInput.setAttribute("required", "");
+	descriptionInput.id = "description-input";
+	descriptionInput.placeholder = "Task Description";
+	createForm.appendChild(descriptionInput);
+
+	const notesInput = document.createElement("input");
+	notesInput.type = "text";
+	notesInput.id = "notes-input";
+	notesInput.placeholder = "Notes";
+	createForm.appendChild(notesInput);
+
+	const addTaskBtn = document.createElement("button");
+	addTaskBtn.id = "add-task-btn";
+	addTaskBtn.className = "btn";
+	addTaskBtn.textContent = "Add";
+	createForm.appendChild(addTaskBtn);
+}
+
 let myProjects = [];
 
 const projectFactory = (title, due, tasks) => {
@@ -48,11 +83,12 @@ function addBtnListener() {
 	const dueInput = document.getElementById("due-input");
 	addBtn.addEventListener("click", (event) => {
 		event.preventDefault();
-		startBtn.style.visibility = "visible";
+		startProjectBtn.style.visibility = "visible";
 
 		const project = projectFactory(titleInput.value, dueInput.value, []);
 		myProjects.push(project);
 		createProjectCard();
+		openProjectListener();
 
 		document.getElementById("create-window").remove();
 	});
@@ -63,7 +99,7 @@ function createProjectCard() {
 	for (let i = 0; i < myProjects.length; i++) {
 		const projectCard = document.createElement("div");
 		projectCard.className = "project-card";
-		projectCon.appendChild(projectCard);
+		projectCon.insertBefore(projectCard, startProjectBtn);
 
 		const cardTitle = document.createElement("h2");
 		cardTitle.textContent = myProjects[i].title;
@@ -73,7 +109,90 @@ function createProjectCard() {
 		cardDue.textContent = myProjects[i].due;
 		projectCard.appendChild(cardDue);
 
-		const cardTasks = document.createElement("span");
-		projectCard.appendChild(cardTasks);
+		/*const cardTasks = document.createElement("span");
+		projectCard.appendChild(cardTasks);*/
+
+		const openProjectBtn = document.createElement("button");
+		openProjectBtn.className = "open-project-btn";
+		openProjectBtn.textContent = "Open Project";
+		projectCard.appendChild(openProjectBtn);
 	}
 }
+
+function openProjectListener() {
+	const openProjectBtn = document.querySelectorAll(".open-project-btn");
+	for (let i = 0; i < openProjectBtn.length; i++) {
+		openProjectBtn[i].addEventListener("click", () => {
+			projectCon.className = "display-none";
+			taskCon.classList.remove("display-none");
+		});
+	}
+}
+//TODO: create a taskFactory and addEventListener to addTaskBtn that pushes the task object to myProjects[0].tasks
+//Then figure out a way to push the task to the correct Project's tasks array
+// ^ (likely data- attribute equal to myProjects[i] given to openProjectBtn)
+//Be kind and best of luck!
+
+const taskFactory = (description, notes) => {
+	return { description, notes };
+};
+
+function addTaskBtnListener() {
+	const addTaskBtn = document.getElementById("add-task-btn");
+	const descriptionInput = document.getElementById("description-input");
+	const notesInput = document.getElementById("notes-input");
+	addTaskBtn.addEventListener("click", (event) => {
+		event.preventDefault();
+		startTaskBtn.style.visibility = "visible";
+
+		const task = taskFactory(descriptionInput.value, notesInput.value);
+		myProjects[0].tasks.push(task);
+		createTaskCard();
+
+		document.getElementById("create-window").remove();
+	});
+}
+
+function createTaskCard() {
+	//TODO: remove myProjects[i].tasks children before loop to avoid repeats <--
+	for (let i = 0; i < myProjects[0].tasks.length; i++) {
+		const taskCard = document.createElement("div");
+		taskCard.className = "task-card";
+		taskCon.insertBefore(taskCard, startTaskBtn);
+
+		const cardDescription = document.createElement("h2");
+		cardDescription.textContent = myProjects[0].tasks[i].description;
+		taskCard.appendChild(cardDescription);
+
+		const cardNotes = document.createElement("span");
+		cardNotes.textContent = myProjects[0].tasks[i].notes;
+		taskCard.appendChild(cardNotes);
+	}
+}
+
+/*myProjects[{
+	"title": "Project 1",
+	"due": "8/30",
+	"tasks": [{
+		"description": "",
+		"notes": ""
+	},
+	{
+		"description": "",
+		"notes": ""
+	}]
+},
+{
+	"title": "Project 2",
+	"due": "9/15",
+	"tasks": [{
+		"description": "",
+		"notes": ""
+	},
+	{
+		"description": "",
+		"notes": ""
+	}]
+}];
+
+myProjects[0].tasks[0].description;*/
