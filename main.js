f56369910cf876ba746d1bebe127bf3648bb80e7
Ian Halcomb <halcombian@gmail.com>
2022-08-27 02:58:13 -0400

    Remove notes property from task objects and add delete buttons for projects and tasks

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//Gets projectCon to be used in createProjectWindow() and createProjectCards()
 const projectCon = document.getElementById("project-con");
+
 const startProjectBtn = document.getElementById("start-project");
 startProjectBtn.addEventListener("click", () => {
 	startProjectBtn.style.visibility = "hidden";
@@ -33,7 +33,6 @@ function createProjectWindow() {
 	const dueInput = document.createElement("input");
 	dueInput.type = "date";
 	dueInput.id = "due-input";
-	dueInput.placeholder = "Due";
 	createForm.appendChild(dueInput);
 
 	const addBtn = document.createElement("button");
@@ -46,29 +45,29 @@ function createProjectWindow() {
 let myProjects = [
 	{
 		title: "Test Project 1",
-		due: "",
+		due: "2022-08-30",
 		tasks: [
-			{ description: "Test 1 Task 1", notes: "" },
-			{ description: "Test 1 Task 2", notes: "" },
-			{ description: "Test 1 Task 3", notes: "" },
+			{ description: "Test 1 Task 1", notes: "Test Note 1" },
+			{ description: "Test 1 Task 2", notes: "Test Note 2" },
+			{ description: "Test 1 Task 3", notes: "Test Note 3" },
 		],
 	},
 	{
 		title: "Test Project 2",
-		due: "",
+		due: "2022-09-15",
 		tasks: [
-			{ description: "Test 2 Task 1", notes: "" },
-			{ description: "Test 2 Task 2", notes: "" },
-			{ description: "Test 2 Task 3", notes: "" },
+			{ description: "Test 2 Task 1", notes: "Test Note 1" },
+			{ description: "Test 2 Task 2", notes: "Test Note 2" },
+			{ description: "Test 2 Task 3", notes: "Test Note 3" },
 		],
 	},
 	{
 		title: "Test Project 3",
-		due: "",
+		due: "2022-09-30",
 		tasks: [
-			{ description: "Test 3 Task 1", notes: "" },
-			{ description: "Test 3 Task 2", notes: "" },
-			{ description: "Test 3 Task 3", notes: "" },
+			{ description: "Test 3 Task 1", notes: "Test Note 1" },
+			{ description: "Test 3 Task 2", notes: "Test Note 2" },
+			{ description: "Test 3 Task 3", notes: "Test Note 3" },
 		],
 	},
 ];
@@ -105,35 +104,66 @@ function createProjectCard() {
 		projectCard.className = "project-card";
 		projectCon.insertBefore(projectCard, startProjectBtn);
 
+		const cardInfoCon = document.createElement("div");
+		cardInfoCon.className = "card-info-con";
+		projectCard.appendChild(cardInfoCon);
+
 		const cardTitle = document.createElement("h2");
 		cardTitle.textContent = myProjects[i].title;
-		projectCard.appendChild(cardTitle);
+		cardInfoCon.appendChild(cardTitle);
+
+		const dueTaskCon = document.createElement("div");
+		dueTaskCon.className = "due-task-con";
+		cardInfoCon.appendChild(dueTaskCon);
 
 		const cardDue = document.createElement("span");
-		cardDue.textContent = myProjects[i].due;
-		projectCard.appendChild(cardDue);
+		cardDue.innerHTML = "<b>Due: </b>" + myProjects[i].due;
+		dueTaskCon.appendChild(cardDue);
+
+		const cardTasks = document.createElement("span");
+		cardTasks.className = "card-tasks";
+		cardTasks.innerHTML = "<b>Tasks: </b>" + myProjects[i].tasks.length;
+		dueTaskCon.appendChild(cardTasks);
 
 		const openProjectBtn = document.createElement("button");
-		openProjectBtn.className = "open-project-btn";
+		openProjectBtn.classList = "open-project-btn btn";
 		openProjectBtn.setAttribute("data-index", i);
 		openProjectBtn.textContent = "Open Project";
 		projectCard.appendChild(openProjectBtn);
+
+		const deleteProjectBtn = document.createElement("button");
+		deleteProjectBtn.classList = "delete-project-btn btn";
+		deleteProjectBtn.setAttribute("data-index", i);
+		deleteProjectBtn.textContent = "Delete";
+		projectCard.appendChild(deleteProjectBtn);
 	}
 	openProjectListener();
+	deleteProjectListener();
 }
 
 function openProjectListener() {
 	const openProjectBtn = document.querySelectorAll(".open-project-btn");
-	for (let i = 0; i < openProjectBtn.length; i++) {
-		openProjectBtn[i].addEventListener("click", () => {
+	openProjectBtn.forEach((btn) => {
+		const index = btn.dataset.index;
+		btn.addEventListener("click", () => {
 			taskCon.classList.remove("display-none");
-			taskCon.setAttribute("data-index", openProjectBtn[i].dataset.index);
+			taskCon.setAttribute("data-index", index);
 			projectCon.className = "display-none";
 
-			document.getElementById("task-h2").innerHTML =
-				"<h2>" + myProjects[i].title + "</h2>";
+			document.getElementById("task-h2").textContent = myProjects[index].title;
 
 			createTaskCard();
+		});
+	});
+}
+
+function deleteProjectListener() {
+	const deleteProjectBtn = document.querySelectorAll(".delete-project-btn");
+	for (let i = 0; i < deleteProjectBtn.length; i++) {
+		deleteProjectBtn[i].addEventListener("click", () => {
+			myProjects.splice(i, 1);
+
+			createProjectCard();
 		});
 	}
 }
@@ -161,12 +191,6 @@ function createTaskWindow() {
 	descriptionInput.placeholder = "Task Description";
 	createForm.appendChild(descriptionInput);
 
-	const notesInput = document.createElement("input");
-	notesInput.type = "text";
-	notesInput.id = "notes-input";
-	notesInput.placeholder = "Notes";
-	createForm.appendChild(notesInput);
-
 	const addTaskBtn = document.createElement("button");
 	addTaskBtn.id = "add-task-btn";
 	addTaskBtn.className = "btn";
@@ -174,19 +198,18 @@ function createTaskWindow() {
 	createForm.appendChild(addTaskBtn);
 }
 
-const taskFactory = (description, notes) => {
-	return { description, notes };
+const taskFactory = (description) => {
+	return { description };
 };
 
 function addTaskBtnListener() {
 	const addTaskBtn = document.getElementById("add-task-btn");
 	const descriptionInput = document.getElementById("description-input");
-	const notesInput = document.getElementById("notes-input");
 	addTaskBtn.addEventListener("click", (event) => {
 		event.preventDefault();
 		startTaskBtn.style.visibility = "visible";
 
-		const task = taskFactory(descriptionInput.value, notesInput.value);
+		const task = taskFactory(descriptionInput.value);
 		myProjects[taskCon.dataset.index].tasks.push(task);
 		createTaskCard();
 
@@ -210,23 +233,28 @@ function createTaskCard() {
 		cardDescription.textContent = myProjects[taskData].tasks[j].description;
 		taskCard.appendChild(cardDescription);
 
-		const cardNotes = document.createElement("span");
-		cardNotes.textContent = myProjects[taskData].tasks[j].notes;
-		taskCard.appendChild(cardNotes);
+		const orderBtnCon = document.createElement("div");
+		taskCard.appendChild(orderBtnCon);
 
 		const orderUpBtn = document.createElement("button");
 		orderUpBtn.textContent = "Up";
-		orderUpBtn.className = "order-up-btn";
+		orderUpBtn.classList = "order-up-btn btn";
 		orderUpBtn.setAttribute("data-index", j);
-		taskCard.appendChild(orderUpBtn);
+		orderBtnCon.appendChild(orderUpBtn);
 
 		const orderDownBtn = document.createElement("button");
 		orderDownBtn.textContent = "Down";
-		orderDownBtn.className = "order-down-btn";
+		orderDownBtn.classList = "order-down-btn btn";
 		orderDownBtn.setAttribute("data-index", j);
-		taskCard.appendChild(orderDownBtn);
+		orderBtnCon.appendChild(orderDownBtn);
+
+		const deleteTaskBtn = document.createElement("button");
+		deleteTaskBtn.classList = "delete-task-btn btn";
+		deleteTaskBtn.textContent = "Delete";
+		taskCard.appendChild(deleteTaskBtn);
 	}
 	orderBtnListener();
+	deleteTaskListener();
 }
 
 function orderBtnListener() {
@@ -238,11 +266,14 @@ function orderBtnListener() {
 	orderUpBtn.forEach((btn) => {
 		const btnData = btn.dataset.index;
 		btn.addEventListener("click", () => {
-			const element = project.tasks.splice(btnData, 1)[0];
-			const index = btnData - 1;
-			project.tasks.splice(index, 0, element);
+			if (btnData == 0) {
+			} else {
+				const element = project.tasks.splice(btnData, 1)[0];
+				const index = btnData - 1;
+				project.tasks.splice(index, 0, element);
 
-			createTaskCard();
+				createTaskCard();
+			}
 		});
 	});
 
@@ -258,5 +289,16 @@ function orderBtnListener() {
 	});
 }
 
-//Called for testing
+function deleteTaskListener() {
+	const deleteTaskBtn = document.querySelectorAll(".delete-task-btn");
+	const taskConIndex = taskCon.dataset.index;
+	for (let i = 0; i < deleteTaskBtn.length; i++) {
+		deleteTaskBtn[i].addEventListener("click", () => {
+			myProjects[taskConIndex].tasks.splice(i, 1);
+
+			createTaskCard();
+		});
+	}
+}
+
 createProjectCard();
